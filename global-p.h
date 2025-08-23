8daa837337f9ab739945bc0c8b632e15b899018a
test <dekamara@protonamil.com>
2025-08-24 01:09:42 +0900

    7

--- global-p.h
+++ global-p.h
@@ -5,10 +5,10 @@
 //符号のパラーメータの指定。通常[N,K,T]として、
 //Nは符号の長さ、Kが符号の次元、Tは訂正エラー数
 //を表す。ここではDは符号長にしている。
-#define N 2053 // set small prime ex. p=2053
-#define M N // puncture code
-#define K (64) // degree of polynomial
-#define E (12)   // bit size of prime
+#define N 7 // set small prime ex. p=2053
+#define M 7 // puncture code
+#define K (4) // degree of polynomial
+#define E (3)   // bit size of prime
 #define DEG N // set (K * E) < N
 #define T (K / 2) // weight of error vector
 #define Q K*2
