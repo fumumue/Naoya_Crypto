79e5008905ec23b43754bbff28b8065d9ad26875
test <dekamara@protonamil.com>
2025-08-29 11:47:04 +0900

    restra

--- hqc_golay.c
+++ hqc_golay.c
@@ -7,6 +7,8 @@
 
 #include "global-p.h"
 #include "struct.h"
+#include "chash-p.c"
+#include "func.c"
 
 
 unsigned int H[11]={
@@ -40,260 +42,6 @@ unsigned int enc(unsigned int y, unsigned int z)
 }
 
 
-// 多項式の次数(default)
-int deg(vec a)
-{
-    int i, n = 0, flg = 0;
-
-    // #pragma omp parallel for
-    for (i = 0; i < DEG; i++)
-    {
-        if (a.x[i] != 0)
-        {
-            n = i;
-            flg = 1;
-        }
-    }
-    if (flg == 0)
-        return 0;
-
-    return n;
-}
-
-
-vec vmul(vec a, vec b,int R)
-{
-    int i, j, k, l;
-    vec c = {0};
-
-    k = deg(a);
-    l = deg(b);
-
-    if(l+k>N){
-        printf("blake %d\n",l+k);
-        exit(1);
-    }
-    i = 0;
-    while (i < k + 1)
-    {
-        for (j = 0; j < l + 1; j++)
-        {
-            if (a.x[i] > 0)
-                c.x[i + j] = (c.x[i + j] + a.x[i] * b.x[j]) % R;
-        }
-        i++;
-    }
-
-    return c;
-}
-
-
-//整数からベクトル型への変換
-vec i2v(unsigned long long n)
-{
-    vec v = {0};
-    int i = 0;
-
-    while (n > 0)
-    {
-        v.x[i++] = n % 2;
-        n = (n >> 1);
-    }
-
-    return v;
-}
-
-//ベクトル型から整数への変換
-unsigned long long v2i(vec v)
-{
-    unsigned long long int d = 0, i;
-
-    for (i = 0; i < 64; i++)
-    {
-        d ^= (v.x[i] << i);
-    }
-    
-    return d;
-}
-
-
-// リーディグタームを抽出(default)
-oterm vLT(vec f)
-{
-    int i;
-    oterm t = {0};
-
-    // k = deg (o2v (f));
-    for (i = 0; i < DEG; i++)
-    {
-        // printf("a=%d %d\n",f.t[i].a,f.t[i].n);
-        if (f.x[i] > 0)
-        {
-            t.n = i;
-            t.a = f.x[i];
-        }
-    }
-
-    return t;
-}
-
-
-
-unsigned long long  gcd(unsigned long long  a, unsigned long long  b)
-{
-  int r, tmp;
-
-  if(b==0 || a==0)
-  return 0;
-  /* 自然数 a > b を確認・入替 */
-  if (a < b)
-  {
-    tmp = a;
-    a = b;
-    b = tmp;
-  }
-
-    //b=N;
-  /* ユークリッドの互除法 */
-  r = (a % b);
-  while (r != 0)
-  {
-    a = b;
-    b = r;
-    r = (a % b);
-  }
-
-  /* 最大公約数を出力 */
-  //printf("最大公約数 = %d\n", b);
-  if(b!=1)
-  {
-    printf("(a,b)!=1\n");
-    return -1;
-  }
-
-  return b;
-}
-
-
-
-// 整数の逆数
-unsigned long long  inv(unsigned long long  a, unsigned long long  n)
-{
-
-    if(gcd(a,n)!=1){
-        printf("(a,n)!=1\n");
-        return -1;
-    }
-    unsigned long long  d = n;
-    unsigned long long  x = 0;
-    unsigned long long  s = 1;
-    while (a != 0)
-    {
-        unsigned long long  q = d / a;
-        unsigned long long  r = d % a;
-        d = a;
-        a = r;
-        unsigned long long  t = x - q * s;
-        x = s;
-        s = t;
-    }
-    unsigned long long  gcd = d; // $\gcd(a, n)$
-
-    return ((x + n) % (n / d));
-}
-
-// aに何をかけたらbになるか
- unsigned long long 
-equ( unsigned long long  a,  unsigned long long  b)
-{
-    // for(unsigned long long  i=0;i<N;i++)
-    if (b == 0)
-        return 0;
-    if (a == 1)
-        return b;
-    if(a==b)
-    return 1;
-
-    return (inv(a, N) * b) % N;
-}
-
-//モニック多項式にする
-vec coeff(vec f,  unsigned long long  d)
-{
-  int i, j, k;
-  vec a, b;
-
-  k = deg((f)) + 1;
-  for (i = 0; i < k; i++)
-    f.x[i] = (f.x[i]*inv(d,N))%N;
-
-  return f;
-}
-
-
-// 多項式を単行式で割る
-oterm vLTdiv(vec f, oterm t)
-{
-    oterm tt = {0}, s = {
-                        0};
-
-    tt = vLT(f);
-    if (tt.n < t.n)
-    {
-        s.n = 0;
-        s.a = 0;
-    }
-    else if (tt.n == t.n)
-    {
-        s.n = 0;
-        s.a = equ(t.a, tt.a);
-    }
-    else if (tt.n > t.n)
-    {
-        s.n = tt.n - t.n;
-        s.a = equ(t.a, tt.a);
-        // printf("%u\n",s.a);
-    }
-    else if (t.n == 0 && t.a > 0)
-    {
-        s.a = (tt.a * inv(t.a, N)) % N;
-        s.n = tt.n;
-    }
-
-    return s;
-}
-
-vec vadd(vec a, vec b)
-{
-    int i;
-    vec c = {0};
-
-    // printf("deg=%d %d\n",deg(a),deg(b));
-
-    for (i = 0; i < DEG; i++)
-        c.x[i] = (a.x[i] + b.x[i]) % N;
-
-    return c;
-}
-
-// 20200816:正規化したいところだがうまく行かない
-// 多項式の足し算
-vec vsub(vec a, vec b)
-{
-    vec c = {0};
-    // int i, j, k, l = 0;
-    vec h = {0}, f2 = {0}, g2 = {0};
-
-    for (int i = 0; i < DEG; i++)
-    {
-        if (a.x[i] >= b.x[i])
-            c.x[i] = (a.x[i] - b.x[i]) % N;
-        if (a.x[i] < b.x[i])
-            c.x[i] = (N + a.x[i] - b.x[i]) % N;
-    }
-
-    return c;
-}
 
 vec xor(vec a,vec b){
     int i=0;
@@ -305,89 +53,6 @@ vec xor(vec a,vec b){
     return c;
 }
 
-// 多項式を項ずつ掛ける
-vec vterml(vec f, oterm t)
-{
-    // f = conv(f);
-    // ssert(op_verify(f));
-    int i;
-    vec h = {0};
-
-    // f=conv(f);
-    // k = deg (o2v(f));
-
-    for (i = 0; i < DEG; i++)
-    {
-        // h.t[i].n = f.t[i].n + t.n;
-        if (f.x[i] > 0)
-            h.x[i + t.n] = (f.x[i] * t.a) % N;
-    }
-
-    // h = conv(h);
-    //  assert(op_verify(h));
-    return h;
-}
-
-//多項式の商を取る
-vec vdiv(vec f, vec g)
-{
-
-  int i = 0, j, n, k;
-  vec h = {0}, e = {0}, tt = {0};
-  oterm a, b = {0}, c = {0};
-
-  if (vLT(f).n == 0 && vLT(g).a == 0)
-  {
-    printf("baka^\n");
-    //return f;
-    exit(1);
-  }
-  if (vLT(g).a == 0)
-  {
-    exit(1);
-  }
-  if (vLT(g).n == 0 && vLT(g).a > 1)
-    return coeff(f, vLT(g).a);
-
-  k = deg(g);
-  b = vLT(g);
-  if (b.a == 1 && b.n == 0)
-    return f;
-  if (b.a == 0 && b.n == 0)
-  {
-    printf("baka in vdiv\n");
-    exit(1);
-  }
-  if (deg((f)) < deg((g)))
-  {
-    return f;
-    //  a=LT(f);
-  }
-
-  i = 0;
-  while (vLT(f).n > 0 && vLT(g).n > 0)
-  {
-    c = vLTdiv(f, b);
-    assert(c.n < DEG);
-    tt.x[c.n] = c.a;
-    //i++;
-
-    h = vterml(g, c);
-
-    f = xor(f, h);
-    if (deg((f)) == 0 || deg((g)) == 0)
-    {
-      //printf ("blake2\n");
-      break;
-    }
-
-    if (c.n == 0)
-      break;
-  }
-
-  // tt は逆順に入ってるので入れ替える
-  return tt;
-}
 
 
 vec conv(vec a,vec b,int n){
@@ -432,16 +97,6 @@ return c;
 }
 
 
-int wt(vec a){
-    int i=0,k=0;
-
-    for(i=0;i<23;i++){
-    if(a.x[i]>0)
-    k++;
-    }
-
-    return k;
-}
 
 unsigned long long sind(unsigned long long a){
 int i,j[11]={0};
@@ -467,7 +122,7 @@ return x;
 /*
     Fisher-Yates shuffle による方法
     配列の要素をランダムシャッフルする
-*/
+
 void random_shuffle(unsigned short *array, size_t size)
 {
   for (size_t i = size; i > 1; --i)
@@ -477,7 +132,7 @@ void random_shuffle(unsigned short *array, size_t size)
     SWAP(int, array[a], array[b]);
   }
 }
-
+*/
 
 void title(void){
     printf("This is Naoya'n Crypto Demo. ");
@@ -524,6 +179,8 @@ void encode(void){
     // about
     x.x[11]=1;
     h.x[5]=1;
+    h.x[13]=1;
+    h.x[19]=1;
     y.x[17]=1;
 
     vec P={0};
@@ -556,7 +213,7 @@ void encode(void){
         }
         printf("a");
     }
-    while(count[2]<1){
+    while(count[2]<2){
         l=rand()%23;
         if(r2.x[l]==0){
         r2.x[l]=1;
@@ -581,9 +238,10 @@ void encode(void){
     vec u=or(r1,conv(h,r2,23)),vv=or(conv(s,r2,23),e);
     vec t=or(vv,conv(u,y,23));
 
-    int n=wt(t),o=wt(or(conv(h,y,23),r2));
+    int n=wt(t),o=wt(t);
 
     printf("\nwt(t)=%d wt(r)=%d wt(s)=%d,wt(u)=%d,wt(v)=%d\n",n,o,wt(s),wt(u),wt(vv));
+    printf("wt(r)=%llb\n",v2i(or(r1,conv(h,r2,23))));
     if(n>0 && n<=3 && wt(u)>0 && wt(vv)>0)
     {
         printf("wr(x)=%d wt(h)=%d wt(r1)=%d,wt(r2)=%d,wt(y)=%d,wt(e)=%d\n",wt(x),wt(h),wt(r1),wt(r2),wt(y),wt(e));
@@ -659,7 +317,7 @@ void encode(void){
     return;
 }
 
-void main(void){
+void kogaitann(void){
 
     encode();
 
--- naoya.c
+++ naoya.c
@@ -11,10 +11,8 @@
 #include <assert.h>
 #include <x86intrin.h> // SIMD命令を使用するためのヘッダファイル
 
-#include "global-p.h"
-#include "struct.h"
-#include "chash-p.c"
 #include "golay.c"
+#include "hqc_golay.c"
 //#include "gcmb.c"
 
 #define SEPARABLE 0
@@ -91,70 +89,6 @@ n=isqrt(n);
 
 return n;
 }
-
-unsigned short gcd(unsigned short a, unsigned short b)
-{
-  int r, tmp;
-
-  if(b==0 || a==0)
-  return 0;
-  /* 自然数 a > b を確認・入替 */
-  if (a < b)
-  {
-    tmp = a;
-    a = b;
-    b = tmp;
-  }
-
-    //b=N;
-  /* ユークリッドの互除法 */
-  r = (a % b);
-  while (r != 0)
-  {
-    a = b;
-    b = r;
-    r = (a % b);
-  }
-
-  /* 最大公約数を出力 */
-  //printf("最大公約数 = %d\n", b);
-  if(b!=1)
-  {
-    printf("(a,b)!=1\n");
-    return -1;
-  }
-
-  return b;
-}
-
-
-// 整数の逆数
-short inv(short a, short n)
-{
-
-    if(gcd(a,n)!=1){
-        printf("(a,n)!=1\n");
-        return -1;
-    }
-    short d = n;
-    short x = 0;
-    short s = 1;
-    while (a != 0)
-    {
-        short q = d / a;
-        short r = d % a;
-        d = a;
-        a = r;
-        short t = x - q * s;
-        x = s;
-        s = t;
-    }
-    short gcd = d; // $\gcd(a, n)$
-
-    return ((x + n) % (n / d));
-}
-
-
  short oinv2(short a, int R)
 {
      short i;
@@ -181,26 +115,6 @@ short inv(short a, short n)
     
     printf("no return2\n");
     exit(1);
-}
-
-// 多項式の次数(default)
-int deg(vec a)
-{
-    int i, n = 0, flg = 0;
-
-    // #pragma omp parallel for
-    for (i = 0; i < DEG; i++)
-    {
-        if (a.x[i] != 0)
-        {
-            n = i;
-            flg = 1;
-        }
-    }
-    if (flg == 0)
-        return 0;
-
-    return n;
 }
 
 // 多項式を表示する(default)
@@ -322,19 +236,6 @@ vec kof2( short c, vec f)
     printpol(h);
     printf(" =h in oinv2\n");
     return h;
-}
-
-vec vadd(vec a, vec b)
-{
-    int i;
-    vec c = {0};
-
-    // printf("deg=%d %d\n",deg(a),deg(b));
-
-    for (i = 0; i < DEG; i++)
-        c.x[i] = (a.x[i] + b.x[i]) % N;
-
-    return c;
 }
 
 vec vadd2(vec a, vec b,int R)
@@ -576,42 +477,6 @@ trace(vec f, unsigned short x)
 }
 
 
-// リーディグタームを抽出(default)
-oterm vLT(vec f)
-{
-    int i;
-    oterm t = {0};
-
-    // k = deg (o2v (f));
-    for (i = 0; i < DEG; i++)
-    {
-        // printf("a=%d %d\n",f.t[i].a,f.t[i].n);
-        if (f.x[i] > 0)
-        {
-            t.n = i;
-            t.a = f.x[i];
-        }
-    }
-
-    return t;
-}
-
-
-// aに何をかけたらbになるか
- short
-equ( short a,  short b)
-{
-    // for(short i=0;i<N;i++)
-    if (b == 0)
-        return 0;
-    if (a == 1)
-        return b;
-    if(a==b)
-    return 1;
-
-    return (inv(a, N) * b) % N;
-}
-
 // aに何をかけたらbになるか
  short
 equ2( short a,  short b,int R)
@@ -628,39 +493,6 @@ equ2( short a,  short b,int R)
     return (inv(a, R) * b) % R;
     
 }
-
-// 多項式を単行式で割る
-oterm vLTdiv(vec f, oterm t)
-{
-    oterm tt = {0}, s = {
-                        0};
-
-    tt = vLT(f);
-    if (tt.n < t.n)
-    {
-        s.n = 0;
-        s.a = 0;
-    }
-    else if (tt.n == t.n)
-    {
-        s.n = 0;
-        s.a = equ(t.a, tt.a);
-    }
-    else if (tt.n > t.n)
-    {
-        s.n = tt.n - t.n;
-        s.a = equ(t.a, tt.a);
-        // printf("%u\n",s.a);
-    }
-    else if (t.n == 0 && t.a > 0)
-    {
-        s.a = (tt.a * inv(t.a, N)) % N;
-        s.n = tt.n;
-    }
-
-    return s;
-}
-
 
 // 多項式を単行式で割る
 oterm vLTdiv2(vec f, oterm t,int R)
@@ -695,29 +527,6 @@ oterm vLTdiv2(vec f, oterm t,int R)
 }
 
 // 多項式を項ずつ掛ける
-vec vterml(vec f, oterm t)
-{
-    // f = conv(f);
-    // ssert(op_verify(f));
-    int i;
-    vec h = {0};
-
-    // f=conv(f);
-    // k = deg (o2v(f));
-
-    for (i = 0; i < DEG; i++)
-    {
-        // h.t[i].n = f.t[i].n + t.n;
-        if (f.x[i] > 0)
-            h.x[i + t.n] = (f.x[i] * t.a) % N;
-    }
-
-    // h = conv(h);
-    //  assert(op_verify(h));
-    return h;
-}
-
-// 多項式を項ずつ掛ける
 vec vterml2(vec f, oterm t,int R)
 {
     // f = conv(f);
@@ -740,24 +549,6 @@ vec vterml2(vec f, oterm t,int R)
     return h;
 }
 
-// 20200816:正規化したいところだがうまく行かない
-// 多項式の足し算
-vec vsub(vec a, vec b)
-{
-    vec c = {0};
-    // int i, j, k, l = 0;
-    vec h = {0}, f2 = {0}, g2 = {0};
-
-    for (int i = 0; i < DEG; i++)
-    {
-        if (a.x[i] >= b.x[i])
-            c.x[i] = (a.x[i] - b.x[i]) % N;
-        if (a.x[i] < b.x[i])
-            c.x[i] = (N + a.x[i] - b.x[i]) % N;
-    }
-
-    return c;
-}
 
 // 20200816:正規化したいところだがうまく行かない
 // 多項式の足し算
@@ -875,34 +666,6 @@ vec vmod2(vec f, vec g,int R)
     // printf("vmod-baka== %d %d\n",deg(f),deg(g));
     return f;
 }
-
-int mul = 0, mul2 = 0;
-vec vmul(vec a, vec b,int R)
-{
-    int i, j, k, l;
-    vec c = {0};
-
-    k = deg(a);
-    l = deg(b);
-
-    if(l+k>N){
-        printf("blake %d\n",l+k);
-        //exit(1);
-    }
-    i = 0;
-    while (i < k + 1)
-    {
-        for (j = 0; j < l + 1; j++)
-        {
-            if (a.x[i] > 0)
-                c.x[i + j] = (c.x[i + j] + a.x[i] * b.x[j]) % R;
-        }
-        i++;
-    }
-
-    return c;
-}
-
 
 
 // 多項式のべき乗
@@ -2303,20 +2066,6 @@ vec ev(vec x,vec v)
         return tx;
 }
 
-
-//モニック多項式にする
-vec coeff(vec f,  short d)
-{
-  int i, j, k;
-  vec a, b;
-
-  k = deg((f)) + 1;
-  for (i = 0; i < k; i++)
-    f.x[i] = (f.x[i]*inv(d,N))%N;
-
-  return f;
-}
-
 //モニック多項式にする
 vec coeff2(vec f,  short d,int R)
 {
@@ -2330,67 +2079,6 @@ vec coeff2(vec f,  short d,int R)
   return f;
 }
 
-
-//多項式の商を取る
-vec vdiv(vec f, vec g)
-{
-
-  int i = 0, j, n, k;
-  vec h = {0}, e = {0}, tt = {0};
-  oterm a, b = {0}, c = {0};
-
-  if (vLT(f).n == 0 && vLT(g).a == 0)
-  {
-    printf("baka^\n");
-    //return f;
-    exit(1);
-  }
-  if (vLT(g).a == 0)
-  {
-    exit(1);
-  }
-  if (vLT(g).n == 0 && vLT(g).a > 1)
-    return coeff(f, vLT(g).a);
-
-  k = deg(g);
-  b = vLT(g);
-  if (b.a == 1 && b.n == 0)
-    return f;
-  if (b.a == 0 && b.n == 0)
-  {
-    printf("baka in vdiv\n");
-    exit(1);
-  }
-  if (deg((f)) < deg((g)))
-  {
-    return f;
-    //  a=LT(f);
-  }
-
-  i = 0;
-  while (vLT(f).n > 0 && vLT(g).n > 0)
-  {
-    c = vLTdiv(f, b);
-    assert(c.n < DEG);
-    tt.x[c.n] = c.a;
-    //i++;
-
-    h = vterml(g, c);
-
-    f = vsub(f, h);
-    if (deg((f)) == 0 || deg((g)) == 0)
-    {
-      //printf ("blake2\n");
-      break;
-    }
-
-    if (c.n == 0)
-      break;
-  }
-
-  // tt は逆順に入ってるので入れ替える
-  return tt;
-}
 
 //多項式の商を取る
 vec vdiv2(vec f, vec g,int R)
@@ -2923,35 +2611,6 @@ static inline unsigned rotr(uint32_t x, uint8_t n)
 	return x >> n | x << (32 - n);
 }
 
-//整数からベクトル型への変換
-vec i2v(unsigned int n)
-{
-    vec v = {0};
-    int i = 0;
-
-    while (n > 0)
-    {
-        v.x[i++] = n % 2;
-        n = (n >> 1);
-    }
-
-    return v;
-}
-
-//ベクトル型から整数への変換
-unsigned v2i(vec v)
-{
-    unsigned long long int d = 0, i, e = 0;
-
-    for (i = 0; i < 32; i++)
-    {
-        e = v.x[i];
-        d ^= (e << i);
-    }
-
-    return d;
-}
-
 /**
  * ascon_permutate - performs the permutation
  *
@@ -3008,16 +2667,6 @@ static void ascon_permutate(struct state *s, uint8_t nr)
 }
 
 
-
-int wt(vec e){
-    int i,count=0;
-
-    for(i=0;i<N;i++)
-    if(e.x[i]>0)
-    count++;
-
-    return count;
-}
 int vor(vec v){
     for(int i=0;i<N;i++){
     if(v.x[256]>0){
